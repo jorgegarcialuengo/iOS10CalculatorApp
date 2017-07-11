801d5b2a4192df8878d16e8545ee13662d797f4b
jorgegarcialuengo <jorgegarcialuengo@gmail.com>
2017-07-11 14:52:31 -0500

    middle lesson 2, working on Model

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
 struct CalculatorBrain {
     //clasees have inheritances, structs do not
     //classes live in the heap, and you have pointers to them, structs don't so they are copyed around
@@ -15,23 +19,45 @@ struct CalculatorBrain {
 
     private var accumulator: Double? // private because it's for internal use only
     
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cons" : Operation.unaryOperation(cos),
+        "±" : Operation.unaryOperation(changeSign)
+    ]
+
+    
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi //display.text = String(Double.pi)   //"\(Double.pi)" could work too
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch  operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation:
+                break
             }
-
-            //let operand = Double(display.text!)!
-            //display.text = String(sqrt(operand))
-            //making some changes
-            
-        default:
-            break
+            //accumulator = constant
         }
-
+        
+        
+//        switch symbol {
+//        case "π":
+//            accumulator = Double.pi //display.text = String(Double.pi)   //"\(Double.pi)" could work too
+//        case "√":
+//            if let operand = accumulator {
+//                accumulator = sqrt(operand)
+//            }
+//            //let operand = Double(display.text!)!
+//            //display.text = String(sqrt(operand))
+//            //making some changes
+//        default:
+//            break
+//        }
     }
     
     mutating func setOperand(_ operand: Double) {
